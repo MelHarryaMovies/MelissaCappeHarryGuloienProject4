78baa769412c2ce74829895c9a789c174fd1ebda
Harry <harryguloien@gmail.com>
2020-11-16 18:02:52 -0500

    first commit with pseudo code :feelsgood:

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//create name space
+myApp = {};
+
+myApp.apiKey = `262b2d458b0315ed4049499ffec1d210`;
+
+//creat init finction 
+
+
+//timeout function on H1 or hover event listener that turns it into a button
+//scroll function on h1 button that scrolls you down to q1
+
+//create function to gather user input and store it in variables
+
+//function to call from the api and pass in user data variables as parameters
+//grab movie poster, title, rating 
+
+
+
+//create display on page function 
