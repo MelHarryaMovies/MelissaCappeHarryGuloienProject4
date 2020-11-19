7525e44a1b7da3eabff477a2b41033d901438afb
Harry <harryguloien@gmail.com>
2020-11-19 17:04:28 -0500

    MORE JAVA SCRIPT SHIT

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 const myApp = {};
 
 myApp.apiKey = `262b2d458b0315ed4049499ffec1d210`;
-myApp.url = `https://api.themoviedb.org/3/discover/movie`
+myApp.url = `https://api.themoviedb.org/3/discover/movie`;
+myApp.posterUrl = `https://image.tmdb.org/t/p/original`;
 
 myApp.romanceId = 10749;
 myApp.actionId = 28;
@@ -14,7 +15,12 @@ myApp.horrorId = 27;
 
 //create init function 
 
-
+myApp.buttonEvent = () => {
+    $('.enter').hide();
+    setTimeout(function() {
+        
+    })
+}
 //timeout function on H1 or hover event listener that turns it into a button
 //scroll function on h1 button that scrolls you down to q1
 
@@ -23,30 +29,47 @@ myApp.horrorId = 27;
 //IF TIE - then present 4th question 
 
 //function to call from the api and pass in user data variables as parameters
-//grab movie poster, title, rating
-myApp.getInfo = $.ajax({
-    url: myApp.url,
-    method: `GET`,
-    datatype: `jsonp`,
-    data: {
-        api_key: myApp.apiKey,
-        language: `en-US`,
-        sort_by: `popularity.desc`,
-        page: 499
-    }
-})
-console.log(myApp.getInfo);
+//grab movie poster, title, overview
+myApp.init = () => {
+    //hide button on load
+    myApp.buttonEvent();
 
-myApp.getInfo.then(function(response) {
-    const movieArray = response.results;
-    console.log(movieArray[0].genre_ids);
-
-    movieArray.forEach(function(movie) {
-        const {genre_ids, title} = movie;
-        console.log(`${genre_ids} film name - ${title}`);
+    //call info from api 
+    myApp.getInfo = $.ajax({
+        url: myApp.url,
+        method: `GET`,
+        datatype: `jsonp`,
+        data: {
+            api_key: myApp.apiKey,
+            language: `en-US`,
+            sort_by: `popularity.desc`,
+            page: 499
+        }
     })
+    console.log(myApp.getInfo);
     
-})
+    myApp.getInfo.then(function(response) {
+        const movieArray = response.results;
+        console.log(movieArray);
+    
+        movieArray.forEach(function(movie) {
+            const {genre_ids, title, poster_path, overview} = movie;
+            console.log(`${title} is about ${overview} and heres a picture ${myApp.posterUrl}${poster_path}`);
+        })
+        
+    })
+
+
+}
+
+
+
+
+
+
+  $(document).ready(function() {
+    myApp.init();
+  });
 
 //create display on page function 
 
